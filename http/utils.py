753cf664ed8b507087200d5bdd826a8d9ad379a9
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-02-17 23:15:45 +0800

    使用生成绝对路径的接口
    替换部分全局变量

--- http/utils.py
+++ http/utils.py
@@ -1,9 +1,24 @@
+from urllib import parse
+from typing import cast
+
 from django.http import HttpRequest
 
-def get_ip(request: HttpRequest) -> 'str | None':
-    x_forwarded_for: str = request.META.get('HTTP_X_FORWARDED_FOR')
+
+def get_ip(request: HttpRequest) -> str | None:
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = cast(str, x_forwarded_for).split(',')[-1].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+def build_full_url(path: str, root: str) -> str:
+    '''构建完整的URL
+
+    将'/path/from/root'转换为'protocol://domain/path/from/root'。
+    如果path已经是完整的URL，则直接返回。
+    '''
+    if not path:
+        return root
+    return parse.urljoin(root.rstrip('/') + '/', path)
